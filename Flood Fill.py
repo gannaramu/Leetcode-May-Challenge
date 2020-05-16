05d2db97d0a815001e90112c8a98a3caa36339d5
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-05-16 02:10:05 -0600

    Day-12

---
+++ Flood Fill.py
@@ -0,0 +1,24 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        
+                
+        def helper(image,x,y,newColor,color):
+            # print(x,y)
+            if(x<0 or x>=len(image) or y<0 or y>=len(image[0])):        
+                return 
+            if(image[x][y]==color):
+                image[x][y]=newColor
+                helper(image,x,y+1,newColor,color)
+                helper(image,x,y-1,newColor,color)
+                helper(image,x+1,y,newColor,color)
+                helper(image,x-1,y,newColor,color)
+            else:
+                return
+          
+        color = image[sr][sc]
+        if color==newColor:
+            return image
+        else:
+            helper(image,sr,sc,newColor,color)
+        return image
